606aa66f1a22849355ec9d524a6a40b4033bd37c
Sudhanva Paturkar <sudhanvapaturkar@Sudhanvas-MacBook-Pro.local>
2022-10-05 22:38:45 +0530

    adding initial solution for kadane's algo

---
+++ kadane.py
@@ -0,0 +1,14 @@
+def getMaxSubArraySum(inputArray):
+    maxSum = 0
+    currentSum = 0
+    for num in inputArray:
+        currentSum += num
+        if currentSum > maxSum:
+            maxSum = currentSum
+        if currentSum < 0:
+            currentSum = 0
+    
+    return maxSum 
+
+inputArray = map(int, list(input("Enter array to get max possible contigous sub array sum:\n").split()))
+print("Max possible contigous sub array sum is: " + str(getMaxSubArraySum(inputArray)))
